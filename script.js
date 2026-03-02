28f4c212575da2e5d333175c7e0cda707f1df1e5
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2026-03-02 13:50:31 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sheetID = "1g0Jromi5ySF25K1T6cylOTCXKS1_AeCCnSGXiyQ7dLM";
 const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json`;
 
-const aiLink = "https://scontent.fdac181-1.fna.fbcdn.net/v/t39.30808-6/571334282_122093701413108275_3133929182357259310_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Af0j8xyzyeYQ7kNvwE6Rid9&_nc_oc=Admr-b74v-hZAPCPTSAW8kt1huBvTY0-Z_-oJO8mFrKx3cERcddpUOfPp2J4gf4xxik&_nc_zt=23&_nc_ht=scontent.fdac181-1.fna&_nc_gid=o3D4GDBPlxYW_yd1apQ8Qw&oh=00_AfulgN2qFSVBc_Eev712z7OTfS61_uu9QrVkticw50gjqg&oe=699A19FE";
+const aiLink = "ai-uftb.jpg";
 
 fetch(sheetURL)
   .then(res => res.text())
